514d7ffb280c6601e463e0c6c769480288430be2
mohini93 <141171355+mohini93@users.noreply.github.com>
2025-12-14 13:10:55 +0530

    Add Search and Bookings components to App

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,1 +1,14 @@
 
+import Search from "./pages/Search";
+import Bookings from "./pages/Bookings";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Flight Booking System</h1>
+      <Search />
+      <hr />
+      <Bookings />
+    </div>
+  );
+}
